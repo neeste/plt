300f561c076d1728da56cd5cd91a911faf4c10b5
Stephen Neely <Stephen.Neely@boystown.org>
2026-08-03 05:15:32 -0500

    Ensure batch script uses current directory

--- build_installer.bat
+++ build_installer.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+cd /d "%~dp0"
 echo ==============================================
 echo       PLT Windows Installer Build Script
 echo ==============================================
